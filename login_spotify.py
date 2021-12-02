dfaa1df7be8ed2223298340babbf60e6e49f0baf
Jonas Werner <72609080+Tubicor@users.noreply.github.com>
2021-12-02 11:50:00 +0100

    Cleaned up first function

--- login_spotify.py
+++ login_spotify.py
@@ -1,6 +1,9 @@
+from logging import lastResort
 from credentials import client_id, client_secrect
-import time
 
+
+import time
+import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -18,23 +21,46 @@ def connect_with_spotipy_to_web_api():
 
     return sp
 
-def get_list_of_all_saved_songs(sp):
-    # with first query get overall number of saved tracks
-    index_saved_tracks = 0
-    max_saved_tracks = -1
-    limit = 50
+def testMethode():
+    pass
+
+def get_list_of_saved_songs(sp, offset=0, total_songs=None):
+
+    # returns list
     list_artist_track = list()
-    while index_saved_tracks < max_saved_tracks or max_saved_tracks == -1:
-        results = sp.current_user_saved_tracks(limit = limit ,offset=index_saved_tracks)
-        if max_saved_tracks == -1:
-            if type(results['total']) != int:
-                raise Exception("Is not int line ~28")
-            max_saved_tracks = int(results['total'])        
+
+    # check for invalid values in parameter
+    if offset <= 0:
+        raise Exception("Wrong offset")
+    if total_songs <= 0:
+        raise Exception("Wrong total songs")
+
+    # index to iterate over saved tracks
+    index = 0 + offset
+    # last index to query
+    last_index = index + total_songs
+    last_index_must_be_set_to_max = False
+    if total_songs is None:
+        last_index_must_be_set_to_max = True
+    
+    #limit query_size to max of 50 (limit of API query) 
+    query_size = total_songs if total_songs < 50 else 50
+
+    # iterate while index is smaller than last index or if last index is yet to be set
+    while index < last_index or last_index_must_be_set_to_max:
+        # query with api for saved songs
+        results = sp.current_user_saved_tracks(limit = query_size, offset=index)
+        # check for max_possible index and correct it to max if given is greater or nothing is given
+        if last_index_must_be_set_to_max or last_index > int(results['total']):
+            last_index = results['total']
+            last_index_must_be_set_to_max = False
+        # iterate over results
         for item in results['items']:
             list_artist_track.append(f"{ item['track']['artists'][0]['name']} - {item['track']['name']}")
-        index_saved_tracks = index_saved_tracks + limit
-        time.sleep(0.01)
-        print("Scanned to Songindex: ", index_saved_tracks)
+        # increment index for possible further querys
+        index = index + query_size
+        time.sleep(0.001) #TODO
+        print("Scanned to Songindex: ", index)
         # Json Format of result --> results['XXXX']
         # href: URL the GET was sent to
         # items: 
@@ -70,6 +96,6 @@ def get_list_of_all_saved_songs(sp):
 
 if __name__ == "__main__":
         sp = connect_with_spotipy_to_web_api()
-        list_songs = get_list_of_all_saved_songs(sp)
+        list_songs = get_list_of_saved_songs(sp, offset=666, total_songs=1)
         print(list_songs)
         save_to_txt("\n".join(list_songs),"saved_songs.txt")
